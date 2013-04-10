db5185b5a9d589d2002d09a8d9081e54b31f8bf0
Gustavo Reyes <gusreyes01@gmail.com>
2013-04-09 23:09:44 -0500

    Buxfix de estadistico

--- urls.py
+++ urls.py
@@ -60,7 +60,7 @@ urlpatterns = patterns('',
      url(r'^SIAV/archivar/', 'app.views.archivar', name = 'archivar'),
 
      url(r'^SIAV/estadistico/(\d+)/$', 'app.views.estadistico', name = 'estadistico'),
-     url(r'^SIAV/estadistico/', 'app.views.estadistico', name = 'estadistico'),
+     #url(r'^SIAV/estadistico/', 'app.views.estadistico', name = 'estadistico'),
 
      url(r'^SIAV/submitted/', 'app.views.submitted', name = 'submitted'),
      
