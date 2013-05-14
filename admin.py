a77a53b2fb7f8edf3044a4b747244be52af049d0
Gustavo <gusreyes01@gmail.com>
2013-05-14 17:19:18 -0600

    Cambios al admin

---
+++ admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from app.models import Avaluo
+
+admin.site.register(Avaluo)
---
+++ app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from app.models import *
+
+admin.site.register(Avaluo)
+admin.site.register(Cliente)
+admin.site.register(Depto)
+admin.site.register(Valuador)
+admin.site.register(Municipio)
+admin.site.register(Estado)
+
