003db03ff65536c1fb87b6b570c18408123a95c8
Gustavo Reyes <gusreyes01@gmail.com>
2013-08-07 21:48:52 -0500

    Bugfix 3

--- app/templatetags/elapsed.py
+++ app/templatetags/elapsed.py
@@ -6,11 +6,12 @@ register = template.Library()
 
 def elapsed(anio):
 	deptos = Depto.objects.all()
+	dias_solicitud = 120
 	how_many_days = 30
 	dias_transcurridos = 0
 	dic = {}
 	for d in deptos:
-		avaluos = Avaluo.objects.filter(Salida__year=anio).filter(Depto=d.depto_id).filter(Salida__gte=datetime.now()-timedelta(days=how_many_days))
+		avaluos = Avaluo.objects.filter(Salida__year=anio).filter(Depto=d.depto_id).filter(Salida__gte=datetime.now()-timedelta(days=how_many_days)).filter(Solicitud__gte=datetime.now()-timedelta(days=dias_solicitud))
 		cantidad_avaluos = avaluos.count()
 		if cantidad_avaluos > 0:
 			for a in avaluos:
