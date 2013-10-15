203c643178103476e0eef08867dbfd77e02731b5
Gustavo Reyes <gusreyes01@gmail.com>
2013-10-15 14:59:57 -0500

    New Relic Cambios

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
+import newrelic.agent
+
+newrelic.agent.initialize('./newrelic.ini')
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SIAV.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program command options
+application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
