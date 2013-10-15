43355cfb2006d69339976da916cb1d4c8ca40150
Gustavo Reyes <gusreyes01@gmail.com>
2013-10-15 17:55:19 -0500

    wsgi.py
    Se cambio la forma en como se manda llamar el wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,10 @@
 import newrelic.agent
+import os
+from django.core.wsgi import get_wsgi_application
 
 newrelic.agent.initialize('./newrelic.ini')
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SIAV.settings")
-
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
 application = newrelic.agent.wsgi_application()(application)
