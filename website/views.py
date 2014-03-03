7633386c91c4c4f27a0466e9628b4de46001787d
Gustavo Reyes <gusreyes01@gmail.com>
2014-03-03 14:45:37 -0600

    Cambios website

--- website/views.py
+++ website/views.py
@@ -7,10 +7,10 @@ def index(request):
     return render_to_response('ericka/index.html', {}, context_instance=RequestContext(request))
 
 def servicios(request):
-    return render_to_response('ericka/servicios.html', {}, context_instance=RequestContext(request))
+    return render_to_response('ericka/services.html', {}, context_instance=RequestContext(request))
 
 def faq(request):
     return render_to_response('ericka/faq.html', {}, context_instance=RequestContext(request))
 
 def contacto(request):
-    return render_to_response('ericka/contacto.html', {}, context_instance=RequestContext(request))
+    return render_to_response('ericka/contact.html', {}, context_instance=RequestContext(request))
