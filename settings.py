757d9e2f337de58b82942acc288050ac5fac290b
Gustavo Reyes <gusreyes01@gmail.com>
2014-05-30 18:47:37 -0500

    Cambios

--- settings.py
+++ settings.py
@@ -96,10 +96,10 @@ STATIC_ROOT = 'C:/inetpub/wwwroot/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-#STATIC_URL = 'http://valnorte.net:100/'
+STATIC_URL = 'http://valnorte.net:100/'
 #STATIC_URL = 'http://localhost:100/'
 # Url para desarrollo
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
